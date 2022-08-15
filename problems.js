6b1e830d776aac66d4b3f115faf379865d1d9087
Zach-Trani <92279998+Zach-Trani@users.noreply.github.com>
2022-08-15 17:22:03 -0400

    attempt with errors

--- problems.js
+++ problems.js
@@ -2,6 +2,14 @@ const logBetween = (lowNum, highNum) => {
   // Define a function logBetween(lowNum, highNum) that will return a new array with every number from lowNum to highNum,
   // inclusive. Inclusive means that the range includes lowNum and highNum.
   // Your code goes here
+  let contains = []
+
+  for (let i = 1; i < highNum; i++){
+    if (i >= lowNum && i <= highNum){
+      contains.push(i)
+    }
+  }
+  return contains
 };
 
 console.log(logBetween(2, 6));
@@ -9,6 +17,16 @@ console.log(logBetween(2, 6));
 const printFives = (max) => {
   // Write a function printFives(max) that return a new array with the multiples of 5 that are less than max.
   // Your code goes here
+  let contains = []
+
+  for (let i = 1; i < max; i++){
+
+    if (i % 5 === 0 && i < max){
+      contains.push(i)
+    
+    }
+  }
+  return contains
 };
 
 console.log(printFives(30));
@@ -18,6 +36,15 @@ const logBetweenStepper = (min, max, step) => {
   // The function should return a new array with numbers between min and max at step intervals.
   // Example logBetweenStepper(1, 10, 2) should return => [3,5,7,9]
   // Your code goes here
+  let contains = []
+
+  for (let i = min; i < max; i+= step){
+    if (i >= min && i <= max){
+      contains.push(i)
+    }
+  }
+  return contains
+
 };
 
 console.log(logBetween(0, 10, 3))
